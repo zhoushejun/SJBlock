6cca5256a404306a2419e2dccaa8bd8762e34848
周社军 <965678322@qq.com>
2017-11-05 14:46:51 +0800

    【新增】SJBlock项目。

---
+++ SJBlock/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SJBlock
+//
+//  Created by 周社军 on 2017/11/5.
+//  Copyright © 2017年 周社军. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
